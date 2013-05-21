ed54f6237048315e44d2a8875808b99b537eaf3f
Dev Mehta <dpmehta02@gmail.com>
2013-05-21 15:37:45 -0700

    completed ex3.2 relational join

---
+++ hw3.2-join.py
@@ -0,0 +1,18 @@
+import MapReduce
+import sys
+
+mr = MapReduce.MapReduce()
+
+def mapper(record):
+  order_id = record[1]
+  mr.emit_intermediate(order_id, record)
+
+def reducer(key, list_of_values):
+  for first_value in list_of_values:
+    for second_value in list_of_values:
+      if first_value[0] == 'order' and second_value[0] == 'line_item':
+        mr.emit(first_value + second_value)
+
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
