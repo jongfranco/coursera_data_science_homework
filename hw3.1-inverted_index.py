927fe0147c176ac7289c2ac756543697d71d50c9
Dev Mehta <dpmehta02@gmail.com>
2013-05-21 11:29:21 -0700

    working inverted index

---
+++ hw3.1-inverted_index.py
@@ -0,0 +1,25 @@
+import MapReduce
+import sys
+
+mr = MapReduce.MapReduce()
+
+def mapper(record):
+    # key: document identifier
+    # value: document contents
+    # used words
+    used = []
+
+    key = record[0]
+    value = record[1]
+    words = value.split()
+    for w in words:
+      if w not in used:
+        mr.emit_intermediate(w, key)
+        used.append(w)
+
+def reducer(key, list_of_values):
+    mr.emit((key, list_of_values))
+
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
