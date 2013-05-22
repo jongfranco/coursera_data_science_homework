5316b464cb755bcb549d2c91fa34789232b2ac03
Dev Mehta <dpmehta02@gmail.com>
2013-05-22 08:57:10 -0700

    completed 3.3/3.4

---
+++ hw3.4-asymmetric_friendships.py
@@ -0,0 +1,24 @@
+import MapReduce
+import sys
+
+mr = MapReduce.MapReduce()
+
+def mapper(record):
+  # emit every combo of friends
+  mr.emit_intermediate(record[0], record[1])
+  mr.emit_intermediate(record[1], record[0])
+
+def reducer(key, list_of_values):
+    # 
+    friends = {}
+    for value in list_of_values:
+        if value in friends:
+          del friends[value]
+        else:
+          friends[value] = 'symmetric'
+    for friend in friends:
+        mr.emit((key, friend))
+
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
