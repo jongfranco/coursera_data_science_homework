452b02f69a2d005f461da7f4f8712ace693cfb47
dpmehta02 <dpmehta02@gmail.com>
2015-01-27 14:47:11 -0800

    refactor

--- exercises/config/credentials.example.py
+++ exercises/config/credentials.example.py
@@ -1,3 +1,7 @@
+"""This is an example credentials file. Replace the keys/secrets in this file
+with your own info, then save that file as credentials.py.
+"""
+
 ACCESS_TOKEN_KEY = "replaceWithYourTwitterAppKey"
 ACCESS_TOKEN_SECRET = "replaceWithYourTwitterAppSecret"
 CONSUMER_KEY = "replaceWithYourTwitterAppKey"
--- exercises/hw1.1-twitterstream.py
+++ exercises/hw1.1-twitterstream.py
@@ -2,39 +2,37 @@
 # -*- coding: utf-8 -*-
 
 """Author: Dev Mehta / dpmehta02@gmail.com
-Description: Hit the Twitter API for sample tweets. Requires updating
-authentication credentials
-Usage: python hw1.1-twitterstream.py
+Description: Hit the Twitter API for sample tweets.
+
+Usage: Updaten config/credentials.py with the appropriate access credentials,
+then run: $ python hw1.1-twitterstream.py
 """
 
 import oauth2 as oauth
 import urllib2 as urllib
 
-from config.credentials import ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET,
-                                CONSUMER_KEY, CONSUMER_SECRET
+from config.credentials import ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 
 
-def fetch_samples():
-    url = "https://stream.twitter.com/1/statuses/sample.json"
-    parameters = []
-    response = twitterreq(url, "GET", parameters)
-    for line in response:
-        print line.strip()
+def create_opener(debug_level=0):
+    """Create HTTP handlers, add them to an opener and then return it."""
+    
+    http_handler = urllib.HTTPHandler(debuglevel=debug_level)
+    https_handler = urllib.HTTPSHandler(debuglevel=debug_level)
+    opener = urllib.OpenerDirector()
+    opener.add_handler(http_handler)
+    opener.add_handler(https_handler)
+    return opener
 
 
-def twitterreq(url, method, parameters):
-    """Construct, sign, and open a twitter request using the hard-coded
-    credentials above.
-    """
+def twitter_request(url, method, parameters):
+    """Construct, sign, and open a twitter request."""
 
     oauth_token = oauth.Token(key=ACCESS_TOKEN_KEY, secret=ACCESS_TOKEN_SECRET)
     oauth_consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
     signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
 
-    _debug_level = 0
     http_method = "GET"
-    http_handler = urllib.HTTPHandler(debuglevel=_debug_level)
-    https_handler = urllib.HTTPSHandler(debuglevel=_debug_level)
 
     req = oauth.Request.from_consumer_and_token(oauth_consumer,
                                                 token=oauth_token,
@@ -52,13 +50,19 @@ def twitterreq(url, method, parameters):
         encoded_post_data = None
         url = req.to_url()
 
-    opener = urllib.OpenerDirector()
-    opener.add_handler(http_handler)
-    opener.add_handler(https_handler)
-
+    _debug_level = 0
+    opener = create_opener(_debug_level)
     response = opener.open(url, encoded_post_data)
 
     return response
+
+
+def fetch_samples():
+    url = "https://stream.twitter.com/1/statuses/sample.json"
+    parameters = []
+    response = twitter_request(url, "GET", parameters)
+    for line in response:
+        print line.strip()
 
 if __name__ == '__main__':
     fetch_samples()
