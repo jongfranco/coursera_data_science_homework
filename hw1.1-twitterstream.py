1e3d440df3a305cfc874ce1ca4fa8d929b81623d
dpmehta02 <dpmehta02@gmail.com>
2015-01-26 16:18:45 -0800

    refactor

--- hw1.1-twitterstream.py
+++ hw1.1-twitterstream.py
@@ -1,61 +1,67 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Author: Dev Mehta / dpmehta02@gmail.com
+Description: ...
+Usage: python scriptName.py
+"""
+
 import oauth2 as oauth
 import urllib2 as urllib
 
-# See Assginment 6 instructions or README for how to get these credentials
-access_token_key = "25025967-kUGYUGg6VJuNipO1yhbYGn5Sq4sx4Gz9tt67T1nSF"
-access_token_secret = "FKfGJbBEXy5gBgCEc4H5xTs9VIDK0nLiO7wuzbUrM8"
 
-consumer_key = "pCnW2Myj1reb1iRrCiIqg"
-consumer_secret = "MdIeFLCdeZQJvQZRnczLy0UiChZ6JeCR4LERpXyPt34"
-
-_debug = 0
-
-oauth_token    = oauth.Token(key=access_token_key, secret=access_token_secret)
-oauth_consumer = oauth.Consumer(key=consumer_key, secret=consumer_secret)
-
-signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
-
-http_method = "GET"
+def fetch_samples():
+    url = "https://stream.twitter.com/1/statuses/sample.json"
+    parameters = []
+    response = twitterreq(url, "GET", parameters)
+    for line in response:
+        print line.strip()
 
 
-http_handler  = urllib.HTTPHandler(debuglevel=_debug)
-https_handler = urllib.HTTPSHandler(debuglevel=_debug)
-
-'''
-Construct, sign, and open a twitter request
-using the hard-coded credentials above.
-'''
 def twitterreq(url, method, parameters):
-  req = oauth.Request.from_consumer_and_token(oauth_consumer,
-                                             token=oauth_token,
-                                             http_method=http_method,
-                                             http_url=url, 
-                                             parameters=parameters)
+    """
+    Construct, sign, and open a twitter request
+    using the hard-coded credentials above.
+    """
 
-  req.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
+    ACCESS_TOKEN_KEY = "25025967-kUGYUGg6VJuNipO1yhbYGn5Sq4sx4Gz9tt67T1nSF"
+    ACCESS_TOKEN_SECRET = "FKfGJbBEXy5gBgCEc4H5xTs9VIDK0nLiO7wuzbUrM8"
+    CONSUMER_KEY = "pCnW2Myj1reb1iRrCiIqg"
+    CONSUMER_SECRET = "MdIeFLCdeZQJvQZRnczLy0UiChZ6JeCR4LERpXyPt34"
 
-  headers = req.to_header()
+    oauth_token = oauth.Token(key=ACCESS_TOKEN_KEY, secret=ACCESS_TOKEN_SECRET)
+    oauth_consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+    signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
 
-  if http_method == "POST":
-    encoded_post_data = req.to_postdata()
-  else:
-    encoded_post_data = None
-    url = req.to_url()
+    _debug_level = 0
+    http_method = "GET"
+    http_handler = urllib.HTTPHandler(debuglevel=_debug_level)
+    https_handler = urllib.HTTPSHandler(debuglevel=_debug_level)
 
-  opener = urllib.OpenerDirector()
-  opener.add_handler(http_handler)
-  opener.add_handler(https_handler)
+    req = oauth.Request.from_consumer_and_token(oauth_consumer,
+                                                token=oauth_token,
+                                                http_method=http_method,
+                                                http_url=url,
+                                                parameters=parameters)
 
-  response = opener.open(url, encoded_post_data)
+    req.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
 
-  return response
+    headers = req.to_header()
 
-def fetchsamples():
-  url = "https://stream.twitter.com/1/statuses/sample.json"
-  parameters = []
-  response = twitterreq(url, "GET", parameters)
-  for line in response:
-    print line.strip()
+    if http_method == "POST":
+        encoded_post_data = req.to_postdata()
+    else:
+        encoded_post_data = None
+        url = req.to_url()
+
+    opener = urllib.OpenerDirector()
+    opener.add_handler(http_handler)
+    opener.add_handler(https_handler)
+
+    response = opener.open(url, encoded_post_data)
+
+    return response
 
 if __name__ == '__main__':
-  fetchsamples()
+    fetch_samples()
